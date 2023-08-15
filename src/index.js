6bf451abbec88fb9ef6060c083be1ad15a2b2872
zhipenglin <sunandmoon001@163.com>
2023-08-15 11:17:10 +0800

    新增其他基础方法导出

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ export {default as useLoader} from './useLoader';
 export {createWithRemoteLoader, default as withRemoteLoader} from './withRemoteLoader';
 export {default} from './RemoteLoader';
 export {preset} from './preset';
+export {getOrLoadRemote} from './getOrLoadRemote';
+export {loadComponent} from './loadComponent';
